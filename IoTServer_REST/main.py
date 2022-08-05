e2b205fb8d9984573b7f8ad7670b69d269238ce1
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2022-08-05 16:08:43 +0200

    Implementation of REST API
    First version of the REST API for the Koper IoT 2022 project

---
+++ IoTServer_REST/main.py
@@ -0,0 +1,109 @@
+import pyfirmata
+import time
+
+# Using flask to make an api
+# import necessary libraries and functions
+from flask import Flask, jsonify, request
+import pyfirmata
+
+app = Flask(__name__)
+global temps, acts, heats, cools, minTs, maxTs
+temps = []
+acts = []
+heats = []
+cools = []
+minTs = []
+maxTs = []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if (request.method == 'GET'):
+        data = "blablabla"
+        return jsonify({'ARDUINO API': data})
+
+@app.route('/temp/<int:temperature>', methods=['GET','PUT'])
+def disp(temperature):
+    temperature = temperature/100
+    if (request.method == 'GET'):
+        print('TEMP GET method called')
+        return jsonify({'temperature_values': temps})
+    elif (request.method == 'PUT'):
+        temps.append(temperature)
+        print("TEMP PUT method called")
+    return jsonify({'result': 'success you put the value somewhere wohooo'})
+
+@app.route('/act/<int:steps>', methods=['GET','PUT'])
+def act(steps):
+    if (request.method == 'GET'):
+        print('ACT GET method called')
+        return jsonify({'activity_values': acts})
+    elif (request.method == 'PUT'):
+        acts.append(steps)
+        print("ACT PUT method called")
+    return jsonify({'result': 'success you put the value somewhere wohooo'})
+
+@app.route('/heat/<int:heat>', methods=['GET','PUT'])
+def heat(heat):
+    if (request.method == 'GET'):
+        print('HEAT GET method called')
+        return jsonify({'heat_values': heats})
+    elif (request.method == 'PUT'):
+        if heat == 1:
+            heat = 'True'
+        else:
+            heat = 'False'
+        heats.append(heat)
+        print("HEAT PUT method called")
+    return jsonify({'result': 'success you put the value somewhere wohooo'})
+
+@app.route('/cool/<int:cool>', methods=['GET','PUT'])
+def cool(cool):
+    if (request.method == 'GET'):
+        print('COOL GET method called')
+        return jsonify({'cool_values': cools})
+    elif (request.method == 'PUT'):
+        if cool == 1:
+            cool = 'True'
+        else:
+            cool = 'False'
+        cools.append(cool)
+        print("COOL PUT method called")
+    return jsonify({'result': 'success you put the value somewhere wohooo'})
+
+
+@app.route('/minT/<int:mint>', methods=['GET','PUT'])
+def mint(mint):
+    if (request.method == 'GET'):
+        print('MINT GET method called')
+        return jsonify({'minT_values': minTs})
+    elif (request.method == 'PUT'):
+        minTs.append(mint)
+        print("MINT PUT method called")
+    return jsonify({'result': 'success you put the value somewhere wohooo'})
+
+@app.route('/maxT/<int:maxt>', methods=['GET','PUT'])
+def maxt(maxt):
+    if (request.method == 'GET'):
+        print('MAXT GET method called')
+        return jsonify({'maxT_values': maxTs})
+    elif (request.method == 'PUT'):
+        maxTs.append(maxt)
+        print("MAXT PUT method called")
+    return jsonify({'result': 'success you put the value somewhere wohooo'})
+
+'''
+        board = pyfirmata.Arduino('com4')
+        print(board)
+        if temperature==1:
+            board.digital[13].write(1)
+        else:
+            board.digital[13].write(0)
+        board.exit()
+'''
+# driver function
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
+
+
