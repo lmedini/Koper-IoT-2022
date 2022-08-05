beb35db1d58698641f41b29d3a370c363bb9c613
Lionel Médini <lionel.medini@univ-lyon1.fr>
2022-08-05 16:03:10 +0200

    temperature sensor client working

--- tempReader-client/index.js
+++ tempReader-client/index.js
@@ -1,12 +1,29 @@
 const { SerialPort } = require('serialport')
+const { ReadlineParser } = require('@serialport/parser-readline')
+const axios = require('axios').default
+
+// TODO: configure the port name here
+const portName = 'COM12'
 
 // Create a port
 const port = new SerialPort({
-  path: 'COM9',
+  path: portName,
   baudRate: 9600,
 })
 
-// Switches the port into "flowing mode"
-port.on('data', function (data) {
-  console.log('Data:', data)
+// Get the temperature value
+// https://serialport.io/docs/api-parsers-overview
+const parser = new ReadlineParser()
+port.pipe(parser)
+
+parser.on('data', (data) => {
+	const url = "http://10.201.60.55:5000/temp/" + 100*data
+	axios({
+	  method: 'put',
+	    url: url
+	});
+	console.log(data + " sent...")
 })
+
+
+//parser.on('data', console.log)
